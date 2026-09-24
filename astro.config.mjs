56f721ed2a4a230fd9bff2ccc7050e0ad6e3cab0
bladgonza <bladgonza@yahoo.com>
2026-09-24 23:07:11 +0200

    Configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://bladgonza.github.io/bgb/',
-  base: process.env.NODE_ENV === 'production' ? '/bgb/' : '/'
+  site: 'https://www.fanclub-bgb.de/',
+  base: '/'
 });
